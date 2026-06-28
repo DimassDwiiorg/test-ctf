5ea1166541b8cbf9fc7999c0c53dd1842eceb00d
DimassDwiiorg <dimassdwisyah@gmail.com>
2026-06-28 12:43:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -201,8 +201,10 @@ def logout():
     flash('Kamu telah logout.', 'info')
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
+
     # Hapus DB lama secara manual di Termux jika skema kolom level tidak terupdate otomatis
     with app.app_context():
         init_db()
+
+    if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
